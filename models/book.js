fa9abbd2eb3cbcb7686ad30ce2b1237138999906
omarfaruk-k <ofkhan30@gmail.com>
2025-09-19 01:21:46 +0600

    post added

---
+++ models/book.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    language:{
+        type: String,
+        required: true
+    },
+    genre:{
+        type: String,
+        required: true
+
+    }
+
+
+})
+
+module.exports = mongoose.model('Book', bookSchema);
--- routes/books.js
+++ routes/books.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/book');
 
-
-router.get('/', (req,res) => {
+router.get('/', async (req,res) => {
     
-     const books = [
-        { id: 1, title: 'Book One', author: 'Author A' },
-        { id: 2, title: 'Book Two', author: 'Author B' }
-    ];
-    res.json(books);
+     try{
+        const books = await Book.find()
+        res.json(books)
+     }catch(err){
+        res.status(500).json({message: err.message});
+     }
+
 })
 
 router.get('/:id', (req,res) => {
     
-    
+    res.send(`Get book with ID ${req.params.id}`);
 })
 
-router.post('/', (req,res) => {
-    
+router.post('/', async (req,res) => {
+   const book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      language: req.body.language,
+      genre: req.body.genre
+   })
+   try {
+      const newBook = await book.save()
+      res.status(201).json(newBook)
+   } catch (error) {
+      res.status.apply(400).json({message: error.message})
+   }
     
 })
 
